02157a7e1bb404fbe7093a4fe82752f53126fe45
João Gabriel Silva <joogabrielsntn66@gmail.com>
2019-02-11 09:20:17 -0300

    Mostrado quantas as vezes o usuario acertou

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-//variavel que guardará os erros que o usuario selecionar
+//variaveis que guardará os erros que o usuario selecionar
 user_erro = " ";
 matriz = [];
+erros = [
+    '4 5',
+    '4 13',
+    '9 2',
+    '11 10',
+    '11 5',
+    '7 9'
+];
 
 function pegarId(id){
     user_erro = id;
@@ -9,16 +17,24 @@ function pegarId(id){
 function clicarBox(){  
     $('input[type=checkbox]').prop('checked', false);
     matriz.push(user_erro);
+
+    for (var i = 0; i < 7; i++){
+        console.log(matriz[i]);
+    }
+
+    compararErros();
 };
 
-console.log(matriz);
-
-erros = [
-    '3 6',
-    '4 13',
-    '9 2'
-];
-
 function compararErros() {
+    let cont = 0;
 
+    for(var i = 0; i < 6; i++) {
+        for(var j = 0; j < 6; j++) {
+            if(matriz[i] == erros[j]){
+                cont++;
+            }
+        }
+    }
+
+    alert("Você acertou "+cont+" vezes");
 }
